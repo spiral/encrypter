6ef2415dbc89c298a6b65c07b46fb0fe53b6d8a1
spiralbot <ci-bot@spiralscout.com>
2024-11-25 20:19:30 +0000

    prepare release

--- tests/EncrypterTest.php
+++ tests/EncrypterTest.php
@@ -10,7 +10,6 @@ use Spiral\Encrypter\Encrypter;
 use Spiral\Encrypter\Exception\DecryptException;
 use Spiral\Encrypter\Exception\EncrypterException;
 
-#[\PHPUnit\Framework\Attributes\CoversClass(\Spiral\Encrypter\Encrypter::class)]
 class EncrypterTest extends TestCase
 {
     public function testImmutable(): void
@@ -24,6 +23,9 @@ class EncrypterTest extends TestCase
         $this->assertEquals($keyB, $new->getKey());
     }
 
+    /**
+     * @covers \Spiral\Encrypter\Encrypter::encrypt
+     */
     public function testEncryption(): void
     {
         $encrypter = new Encrypter(Key::CreateNewRandomKey()->saveToAsciiSafeString());
